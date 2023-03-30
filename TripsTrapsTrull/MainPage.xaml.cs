69443c1e7319275d32956e5f24846590fd21330e
Эдгар Неверовский <edgarneverovskij@gmail.com>
2023-03-30 20:09:53 +0300

    Rules updating, but not working

--- TripsTrapsTrull/MainPage.xaml.cs
+++ TripsTrapsTrull/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,9 @@ namespace TripsTrapsTrull
         Grid grid = new Grid();
         int taps = 0;
         List<Image> images = new List<Image>();
+        Image cross = new Image { Source = ImageSource.FromFile("cross.png") };
+        Image circle = new Image { Source = ImageSource.FromFile("circle.png") };
+        
         public MainPage()
         {
             Label label = new Label 
@@ -39,7 +43,7 @@ namespace TripsTrapsTrull
             Image img = (Image)sender;
             if (taps % 2 == 0)
             {
-                img.Source = ImageSource.FromFile("circle.png");
+                img.Source = ImageSource.FromFile("circle.png");                
             }
             else
             {
@@ -59,12 +63,12 @@ namespace TripsTrapsTrull
         {
             images.Clear();
             grid.Children.Clear();
-            for (int i = 0; i <= 2; i++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j <= 2; j++)
+                for (int j = 0; j < 3; j++)
                 {
                     Image image = new Image
-                    {
+                    {                        
                         HeightRequest = 200,
                         WidthRequest = 200,
                         Source = ImageSource.FromFile("white.png")
@@ -75,7 +79,7 @@ namespace TripsTrapsTrull
                     images.Add(image);
 
                     //Frame frame = new Frame
-                    //{
+                    //{                        
                     //    WidthRequest = 200,
                     //    HeightRequest = 200,
                     //    BorderColor = Color.Black,
@@ -94,38 +98,34 @@ namespace TripsTrapsTrull
         }
         async void Voidu_kontroll()
         {
-            //if (img.AutomationId == ) {}
-            ImageSource cross = ImageSource.FromFile("cross.png");
-            ImageSource circle = ImageSource.FromFile("circle.png");
-
-            List<ImageSource> sources = new List<ImageSource> { cross, circle };
-
-
-
-            //Оптимизация правил (только с ноликом)
+            List<string> sources = new List<string> { cross.ToString(), circle.ToString() };
+            //Оптимизация правил 
             //Next level оптимизация под списки >>> в for длина Math.Sqrt(images.Count) + создание переменной, считающая совпадения
-            //for (int i = 0; i < 3; i += 3) //Выигрыш в ряд
-            //{
-            //    if (images[i].Source == circle.Source && images[i + 1].Source == circle.Source && images[i + 2].Source == circle.Source)
-            //    {
-            //        await DisplayAlert("WIN", "Second on võitnud", "OK");
-            //    }
-            //}
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    if (images[i].Source == circle.Source && images[i + 3].Source == circle.Source && images[i + 6].Source == circle.Source)
-            //    {
-            //        await DisplayAlert("WIN", "Second on võitnud", "OK");
-            //    }
-            //}
-            //if (images[0].Source == circle.Source && images[4].Source == circle.Source && images[8].Source == circle.Source ||
-            //    images[6].Source == circle.Source && images[4].Source == circle.Source && images[2].Source == circle.Source)
-            //{
-            //    await DisplayAlert("WIN", "Second on võitnud", "OK");
-            //}
-            
+            for (int i = 0; i < sources.Count; i++)
+            {                
+                for (int j = 0; j < 3; j += 3) //Выигрыш по ряду
+                {
+                    if (images[j].ToString() == sources[i] && images[j + 1].ToString() == sources[i] && images[j + 2].ToString() == sources[i])
+                    {
+                        await DisplayAlert("WIN", sources[i] + " on võitnud", "OK");
+                    }
+                }               
+                //for (int j = 0; j < 3; j++) //Выигрыш по колонке
+                //{
+                //    if (images[j].Source == sources[i] && images[j + 3].Source == sources[i] && images[j + 6].Source == sources[i])
+                //    {
+                //        await DisplayAlert("WIN", sources[i].ToString() + " on võitnud", "OK");
+                //    }
+                //}
+                ////Выигрыш по диагонали
+                //if (images[0].Source == sources[i] && images[4].Source == sources[i] && images[8].Source == sources[i] ||
+                //    images[6].Source == sources[i] && images[4].Source == sources[i] && images[2].Source == sources[i])
+                //{
+                //    await DisplayAlert("WIN", sources[i].ToString() + " on võitnud", "OK");
+                //}
+            }
 
-            //}
+
             //Если в ячейках
             //1, 2, 3; >>> 0, 1, 2
             //4, 5, 6; >>> 3, 4, 5
@@ -140,7 +140,7 @@ namespace TripsTrapsTrull
             //одинаковые картинки, то игра выиграна
 
             //идея со списком >>> не работает
-            //идея содержит ли список из 2 картинок (прошлую идею)
+            //идея содержит ли список из 2 картинок (прошлую идею) >>> не работает
             //идея с двумерным массивом и передачей информации местоположения (ряд, колонка)
         }
     }
