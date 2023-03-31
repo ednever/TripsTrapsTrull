b7a5109aa710c7dbae86d4486192b548aff3964f
Эдгар Неверовский <edgarneverovskij@gmail.com>
2023-03-31 19:58:43 +0300

    Очень много обновлений)

--- TripsTrapsTrull/MainPage.xaml.cs
+++ TripsTrapsTrull/MainPage.xaml.cs
@@ -12,57 +12,140 @@ namespace TripsTrapsTrull
     public partial class MainPage : ContentPage
     {
         Grid grid = new Grid();
-        int taps = 0;
-        List<Image> images = new List<Image>();
+        Random rnd = new Random();               
         Image cross = new Image { Source = ImageSource.FromFile("cross.png") };
         Image circle = new Image { Source = ImageSource.FromFile("circle.png") };
+        Image white = new Image { Source = ImageSource.FromFile("white.png") };
+
+        bool arvuti = false;
+        int voit = 0;
+        int taps = 0;
+        int red = 0;
+        int blue = 0;
+
         List<string> sources;
-
-
+        List<Image> images = new List<Image>();
+        List<Label> labels = new List<Label>();
+        List<string> labelsNames = new List<string> { "Trips Traps Trull", "0", "vs", "0" };
+        List<Button> buttons = new List<Button>();
+        List<string> buttonsNames = new List<string> { "Uus mäng", "Mäng arvuti vastu", "X", "O"};
+        List<StackLayout> layouts = new List<StackLayout>();
         public MainPage()
         {
-            Label label = new Label 
-            { 
-                Text = "Trips Traps Trull", 
-                FontSize = 20, 
-                TextColor = Color.Black, 
-                HorizontalOptions = LayoutOptions.Center, 
-                VerticalOptions = LayoutOptions.Center 
-            };            
+            //Создание StackLayot
+            for (int i = 0; i < 2; i++)
+            {
+                StackLayout st1 = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center
+                };
+                layouts.Add(st1);
+            }
+
+            //Создание Label и Button
+            for (int i = 0; i < labelsNames.Count; i++)
+            {
+                Label label = new Label
+                {
+                    Text = labelsNames[i],
+                    FontSize = 20,
+                    TextColor = Color.Black,
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center
+                };
+                labels.Add(label);
+                layouts[0].Children.Add(label); //Добавление Label в вверхний StackLayout
+
+                Button button = new Button
+                {
+                    Text = buttonsNames[i],
+                    FontSize = 20,
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center
+                };
+                button.Clicked += Button_Clicked;
+                buttons.Add(button);
+                layouts[1].Children.Add(button); //Добавление Button в нижний StackLayout
+            }
+
+            layouts[0].Children.Remove(labels[0]); //Удаление Заголовка из вверхнего StackLayout
+            layouts[0].Children.Insert(0, buttons[2]); //Добавление Button "X" в вверхний StackLayout
+            layouts[0].Children.Insert(4, buttons[3]); //Добавление Button "O" в вверхний StackLayout
+
+            layouts[1].Children.Remove(buttons[2]); //Удаление Button "X" из нижнего StackLayout
+            layouts[1].Children.Remove(buttons[3]); //Удаление Button "O" из нижнего StackLayout
+
+            StackLayout st = new StackLayout { Children = { labels[0], layouts[0], grid, layouts[1] } };
+            Content = st;            
 
             Uus_mang();
-
-            Button button = new Button { Text = "Uus mäng" };
-            button.Clicked += Button_Clicked;
-
-            StackLayout st = new StackLayout { Children = { label, grid, button } };
-            Content = st;
         }
-
         void Tap_Tapped(object sender, EventArgs e)
         {
+            buttons[2].IsEnabled = false;
+            buttons[3].IsEnabled = false;
+
             taps++;
             Image img = (Image)sender;
             if (taps % 2 == 0)
             {
-                img.Source = ImageSource.FromFile("circle.png");                
+                img.Source = ImageSource.FromFile("circle.png");
+                buttons[3].BackgroundColor = Color.LightGray;
+                buttons[2].BackgroundColor = Color.Gray;
             }
             else
             {
                 img.Source = ImageSource.FromFile("cross.png");
+                buttons[2].BackgroundColor = Color.LightGray;
+                buttons[3].BackgroundColor = Color.Gray;
             }
             img.IsEnabled = false;
 
+            Arvuti(arvuti);
             Voidu_kontroll();
         }
-
         void Button_Clicked(object sender, EventArgs e)
-        {
-            Uus_mang();
+        {            
+            Button btn = (Button)sender;
+            if (btn == buttons[0] || btn == buttons[1])
+            {
+                Uus_mang();
+                if (btn == buttons[1])
+                {
+                    arvuti = true;
+                    Arvuti(arvuti);
+                }
+            }
+            else if (btn == buttons[2])
+            {
+                buttons[2].IsEnabled = false;
+                buttons[3].IsEnabled = true;
+                buttons[3].BackgroundColor = Color.LightGray;
+                buttons[2].BackgroundColor = Color.Gray;
+                taps = 0;
+            }
+            else
+            {
+                buttons[3].IsEnabled = false;
+                buttons[2].IsEnabled = true;
+                buttons[2].BackgroundColor = Color.LightGray;
+                buttons[3].BackgroundColor = Color.Gray;
+                taps = 1;
+            }
         }
-
         void Uus_mang()
         {
+            for (int i = 2; i <= 3; i++)
+            {
+                buttons[i].IsEnabled = true;
+                buttons[i].BackgroundColor = Color.LightGray;
+            }
+            buttons[2].BackgroundColor = Color.Gray;
+
+            arvuti = false;
+            voit = 0;
             taps = 0;
             images.Clear();
             grid.Children.Clear();
@@ -74,24 +157,14 @@ namespace TripsTrapsTrull
                     {                        
                         HeightRequest = 200,
                         WidthRequest = 200,
-                        Source = ImageSource.FromFile("white.png")
+                        Source = ImageSource.FromFile("white.png"),
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Center,
                     };
                     TapGestureRecognizer tap = new TapGestureRecognizer();
                     tap.Tapped += Tap_Tapped;
                     image.GestureRecognizers.Add(tap);
                     images.Add(image);
-
-                    //Frame frame = new Frame
-                    //{                        
-                    //    WidthRequest = 200,
-                    //    HeightRequest = 200,
-                    //    BorderColor = Color.Black,
-                    //    Content = circle
-                    //};
-
-                    //grid.Children.Add(frame);
-                    //Grid.SetRow(frame, i);
-                    //Grid.SetColumn(frame, j);
 
                     grid.Children.Add(image);
                     Grid.SetRow(image, i);
@@ -109,50 +182,108 @@ namespace TripsTrapsTrull
                 {
                     if (images[i].Source.ToString() == item && images[i + 1].Source.ToString() == item && images[i + 2].Source.ToString() == item)
                     {
-                        Voit(item);
+                        voit++;
+                        Voit(item, voit);
                     }
                 }
                 for (int i = 0; i < 3; i++) //Выигрыш по колонке
                 {
                     if (images[i].Source.ToString() == item && images[i + 3].Source.ToString() == item && images[i + 6].Source.ToString() == item)
                     {
-                        Voit(item);
+                        voit++;
+                        Voit(item, voit);
                     }
                 }
                 //Выигрыш по диагонали
                 if (images[0].Source.ToString() == item && images[4].Source.ToString() == item && images[8].Source.ToString() == item ||
                     images[6].Source.ToString() == item && images[4].Source.ToString() == item && images[2].Source.ToString() == item)
                 {
-                    Voit(item);
+                    voit++;
+                    Voit(item, voit);
                 }
             }
 
+            bool vabaKoht = true;
+            foreach (Image item in images)
+            {
+                if (item.Source.ToString() == white.Source.ToString())
+                {
+                    vabaKoht = false;
+                }
+            }
+            if (vabaKoht)
+            {
+                voit++;
+                string mitteKeegi = string.Empty;
+                Voit(mitteKeegi, voit);
+            }
+
+
             //Если в ячейках
-            //1, 2, 3; >>> 0, 1, 2
-            //4, 5, 6; >>> 3, 4, 5
-            //7, 8, 9; >>> 6, 7, 8
+            //1, 2, 3 >>> 0, 1, 2
+            //4, 5, 6 >>> 3, 4, 5
+            //7, 8, 9 >>> 6, 7, 8
             //
-            //1, 4, 7; >>> 0, 3, 6
-            //2, 5, 8; >>> 1, 4, 7
-            //3, 6, 9; >>> 2, 5, 8
+            //1, 4, 7 >>> 0, 3, 6
+            //2, 5, 8 >>> 1, 4, 7
+            //3, 6, 9 >>> 2, 5, 8
             //
-            //1, 5, 9 >>> 0, 4, 8  >>> 0, (0 - 4)
-            //7, 5, 3 >>> 6, 4, 2  >>> 6, (6 - 4)
+            //1, 5, 9 >>> 0, 4, 8  
+            //7, 5, 3 >>> 6, 4, 2 
             //одинаковые картинки, то игра выиграна
 
             //Next level оптимизация под списки >>> в for длина Math.Sqrt(images.Count) + создание переменной, считающая совпадения
         }
-        async void Voit(string voitja)
-        {            
-            if (sources.IndexOf(voitja) == 0)
+        async void Voit(string voitja, int onoff2)
+        {
+            if (onoff2 == 1)
             {
-                await DisplayAlert("Õnnitlus", "Ristid võitsid!", "OK");
+                if (sources.IndexOf(voitja) == 0)
+                {
+                    await DisplayAlert("Õnnitlus", "Ristid võitsid!", "OK");
+                    red++;
+                    labels[1].Text = red.ToString();
+                }
+                else if (sources.IndexOf(voitja) == 1)
+                {
+                    await DisplayAlert("Õnnitlus", "Nullid võitsid!", "OK");
+                    blue++;
+                    labels[3].Text = blue.ToString();
+                }
+                else
+                {
+                    await DisplayAlert("Õnnitlus", "Viik!", "OK");
+                }
+                Uus_mang();
             }
-            else
+        }
+        async void Arvuti(bool onoff) //Из-за паузы можно нажать быстрее бота. Бот ставит только нолики
+        {
+            //Next Level - бот, который всегда выигрывает
+            if (onoff)
             {
-                await DisplayAlert("Õnnitlus", "Nullid võitsid!", "OK");
+                if (taps % 2 != 0)
+                {
+                    List<Image> vabadKohad = new List<Image>();
+                    foreach (Image item in images)
+                    {
+                        if (item.Source.ToString() == white.Source.ToString())
+                        {
+                            vabadKohad.Add(item);
+                        }
+                    }
+                    if (vabadKohad.Count > 1)
+                    {
+                        int a = rnd.Next(vabadKohad.Count);
+                        await Task.Delay(1000);
+                        vabadKohad[a].Source = circle.Source;
+                        vabadKohad[a].IsEnabled = false;
+                        vabadKohad.Clear();
+                        taps++;
+                    }
+                    Voidu_kontroll();
+                }
             }            
-            Uus_mang();
         }
     }
 }
